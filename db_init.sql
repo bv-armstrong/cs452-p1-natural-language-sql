43e2a2e91e0d1e72071254a341fa8031198d0d81
Bryson Armstrong <armstr24@byu.edu>
2024-02-03 17:48:44 -0700

    defined database

---
+++ db_init.sql
@@ -0,0 +1,48 @@
+create table if not exists product_type (
+    product_type_id int primary key,
+    display_value varchar(20) not null
+);
+
+create table if not exists product (
+    product_id int primary key,
+    description varchar(50),
+    product_type_id int,
+    foreign key (product_type_id) references product_type (product_type_id)
+);
+
+create table if not exists vending_machine (
+    vending_machine_id int primary key,
+    longitude decimal(9,6) not null,
+    latitude decimal(8,6) not null
+);
+
+create table if not exists vending_machine_product (
+    vending_machine_id int,
+    product_id int,
+    quantity int,
+    price decimal(3,2),
+    primary key (vending_machine_id, product_id),
+    foreign key (vending_machine_id) references vending_machine (vending_machine_id),
+    foreign key (product_id) references product (product_id)
+);
+
+create table if not exists user (
+    user_id int primary key,
+    name varchar(20)
+);
+
+create table if not exists user_vending_machine_starred (
+    user_id int,
+    vending_machine_id int,
+    primary key (user_id, vending_machine_id),
+    foreign key (user_id) references user (user_id),
+    foreign key (vending_machine_id) references vending_machine (vending_machine_id)
+);
+
+create table if not exists user_product_starred (
+    user_id int,
+    product_id int,
+    primary key (user_id, product_id),
+    foreign key (user_id) references user (user_id),
+    foreign key (product_id) references product (product_id)
+);
---
+++ db_init_data.sql
@@ -0,0 +1,44 @@
+insert into product_type (product_type_id, display_value) values 
+(1, "Drink"),
+(2, "Snack");
+
+insert into product (product_id, description, product_type_id) values 
+(1, "Coke", 1),
+(2, "Diet Coke", 1),
+(3, "Lemonade", 1),
+(4, "Chips", 2),
+(5, "Bagel", null);
+
+insert into vending_machine (vending_machine_id, longitude, latitude) values 
+(1, -111.649315, 40.251842),
+(2, -111.649178, 40.248305),
+(3, -111.647095, 40.248600);
+
+insert into vending_machine_product (vending_machine_id, product_id, quantity, price) values 
+(1, 1, 3, 1.25),
+(1, 2, 5, 1.25),
+(1, 4, 0, 1.00),
+(1, 5, 2, 2.50),
+(2, 1, 1, 1.50),
+(2, 2, 4, 1.50),
+(2, 3, 2, 1.50),
+(3, 3, 0, 1.00),
+(3, 4, 1, 0.75),
+(3, 4, 6, 1.50);
+
+insert into user (user_id, name) values 
+(1, "John"),
+(2, "Mary"),
+(3, "Bob");
+
+insert into user_vending_machine_starred (user_id, vending_machine_id) values 
+(1, 1),
+(1, 2),
+(2, 3);
+
+insert into user_product_starred (user_id, product_id) values 
+(1, 1),
+(1, 2),
+(1, 5),
+(2, 3),
+(2, 5);
